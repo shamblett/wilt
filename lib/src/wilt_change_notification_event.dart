2fdf743b3e0ee962390ef33242d6ed99ecf1e07d
Steve Hamblett <steve.hamblett@linux.com>
2025-04-28 11:18:06 +0100

    Issue 39 - DCM fixes

--- lib/src/wilt_change_notification_event.dart
+++ lib/src/wilt_change_notification_event.dart
@@ -18,6 +18,107 @@ part of '../wilt.dart';
 /// See each event below for a description of its effect.
 class WiltChangeNotificationEvent {
   /// Update event
+  ///
+  /// A document has been created/updated
+  static const String updatee = 'update';
+
+  /// Delete event
+  ///
+  /// A document has been deleted
+  static const String deletee = 'delete';
+
+  /// Decode error event
+  ///
+  /// The change notification received from CouchDB cannot be
+  /// transformed into valid JSON, this may be a temporary issue,
+  /// the client must decide whether to continue on or re-initialise
+  /// the change notification class.
+  static const String decodeErrorr = 'decode';
+
+  /// CouchDb error event
+  ///
+  /// The change notification received from CouchDB indicates an error.
+  static const String couchdbError = 'couch';
+
+  /// Abort event
+  ///
+  /// A fatal error in the HTTP client has been detected, the client must
+  /// assume that on receiving the event the change notification interface must
+  /// be re-initialised.
+  static const String abortt = 'abort';
+
+  /// Last sequence number event
+  ///
+  /// Sent by CouchDB when there are no changes, only the last sequence
+  /// number is returned.
+  ///
+  static const String lastSequence = 'sequence';
+
+  String? _type;
+
+  dynamic _sequenceNumber;
+
+  String? _docId;
+
+  String? _docRevision;
+
+  jsonobject.JsonObjectLite<dynamic>? _document;
+
+  String? _exception;
+
+  String? _httpResponseText;
+
+  String? _couchError;
+
+  String? _couchReason;
+
+  /// Type
+  ///
+  /// Valid for all events
+  String? get type => _type;
+
+  /// Sequence Number
+  ///
+  /// Valid for update, delete and sequence
+  ///
+  dynamic get sequenceNumber => _sequenceNumber;
+
+  /// Document identifier
+  ///
+  /// Valid for update and delete
+  String? get docId => _docId;
+
+  /// Document revision
+  ///
+  /// Valid for update and delete but optional
+  String? get docRevision => _docRevision;
+
+  /// Document object
+  ///
+  /// Valid for update and only if includeDocs is true
+  jsonobject.JsonObjectLite<dynamic>? get document => _document;
+
+  /// Exception string
+  ///
+  /// Valid for abort and decode error
+  String? get exception => _exception;
+
+  /// HTTP response text
+  ///
+  /// Valid for decode error
+  String? get httpResponseText => _httpResponseText;
+
+  /// Couch error
+  ///
+  /// Valid for update and delete
+  String? get couchError => _couchError;
+
+  /// Couch reason
+  ///
+  /// Valid for update and delete
+  String? get couchReason => _couchReason;
+
+  /// Update event
   WiltChangeNotificationEvent.update(
     this._docId,
     this._docRevision,
@@ -61,105 +162,4 @@ class WiltChangeNotificationEvent {
   WiltChangeNotificationEvent.sequence(this._sequenceNumber) {
     _type = lastSequence;
   }
-
-  /// Update event
-  ///
-  /// A document has been created/updated
-  static const String updatee = 'update';
-
-  /// Delete event
-  ///
-  /// A document has been deleted
-  static const String deletee = 'delete';
-
-  /// Decode error event
-  ///
-  /// The change notification received from CouchDB cannot be
-  /// transformed into valid JSON, this may be a temporary issue,
-  /// the client must decide whether to continue on or re-initialise
-  /// the change notification class.
-  static const String decodeErrorr = 'decode';
-
-  /// CouchDb error event
-  ///
-  /// The change notification received from CouchDB indicates an error.
-  static const String couchdbError = 'couch';
-
-  /// Abort event
-  ///
-  /// A fatal error in the HTTP client has been detected, the client must
-  /// assume that on receiving the event the change notification interface must
-  /// be re-initialised.
-  static const String abortt = 'abort';
-
-  /// Last sequence number event
-  ///
-  /// Sent by CouchDB when there are no changes, only the last sequence
-  /// number is returned.
-  ///
-  static const String lastSequence = 'sequence';
-
-  String? _type;
-
-  /// Type
-  ///
-  /// Valid for all events
-  String? get type => _type;
-
-  dynamic _sequenceNumber;
-
-  /// Sequence Number
-  ///
-  /// Valid for update, delete and sequence
-  ///
-  dynamic get sequenceNumber => _sequenceNumber;
-
-  String? _docId;
-
-  /// Document identifier
-  ///
-  /// Valid for update and delete
-  String? get docId => _docId;
-
-  String? _docRevision;
-
-  /// Document revision
-  ///
-  /// Valid for update and delete but optional
-  String? get docRevision => _docRevision;
-
-  jsonobject.JsonObjectLite<dynamic>? _document;
-
-  /// Document object
-  ///
-  /// Valid for update and only if includeDocs is true
-  jsonobject.JsonObjectLite<dynamic>? get document => _document;
-
-  String? _exception;
-
-  /// Exception string
-  ///
-  /// Valid for abort and decode error
-  String? get exception => _exception;
-
-  String? _httpResponseText;
-
-  /// HTTP response text
-  ///
-  /// Valid for decode error
-  String? get httpResponseText => _httpResponseText;
-
-  String? _couchError;
-
-  /// Couch error
-  ///
-  /// Valid for update and delete
-  String? get couchError => _couchError;
-
-  String? _couchReason;
-
-  /// Couch reason
-  ///
-  /// Valid for update and delete
-  String? get couchReason => _couchReason;
 }
